4623b616b3b5dca193fb3ec0071ea5af26610809
Frank Wang <frankw084@berkeley.edu>
2019-08-10 15:22:35 -0700

    create_message function written

--- send_email.py
+++ send_email.py
@@ -20,3 +20,13 @@ try:
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
+
+class send_email:
+    def __init__(self, service):
+        self.service = service
+    def create_message(self, sender, to, subject, message_text):
+        message = MIMEText(message_text)
+        message['to'] = to
+        message['from'] = sender
+        message['subject'] = subject
+        return {'rw': base64.urlsafe_b64encode(message.as_bytes())}
