5536c259363677d7ab83cfca7a8c785a28ca535f
Frank Wang <frankw084@berkeley.edu>
2019-08-21 01:06:09 -0700

    existence of a listed price is checked

--- craigslist_get.py
+++ craigslist_get.py
@@ -7,6 +7,8 @@ def get_posts_on_page(url):
     posts = html_soup.find_all('li', class_= 'result-row')
     return posts
 def get_price(post):
-    return int(post.a.text[2:])
+    if len(post.a.text[2:]) > 0: #Checking if a price was listed
+        return int(post.a.text[2:])
+    return 99999999 #If there is no price listed, do not consider
 def get_description(post):
     return post.find('a', class_='result-title hdrlnk').text
