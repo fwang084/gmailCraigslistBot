e7aeca5c53e6fce1d8fb8a38a5e280642e191c3c
Frank Wang <frankw084@berkeley.edu>
2019-08-21 00:43:57 -0700

    get_posts_on_page function now takes a url

--- craigslist_get.py
+++ craigslist_get.py
@@ -2,7 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 
 def get_posts_on_page(url):
-    response = get('https://sfbay.craigslist.org/search/eby/fua')
+    response = get(url)
     html_soup = BeautifulSoup(response.text, 'html.parser')
     posts = html_soup.find_all('li', class_= 'result-row')
     return posts
