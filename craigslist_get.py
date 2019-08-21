9f5aaafc10664a8c623d3ee469a685cd2f88ef5d
Frank Wang <frankw084@berkeley.edu>
2019-08-21 00:41:10 -0700

    functions written to extract info from posts

--- craigslist_get.py
+++ craigslist_get.py
@@ -1,9 +1,12 @@
 from requests import get
-
-response = get('https://sfbay.craigslist.org/search/eby/fua')
 from bs4 import BeautifulSoup
-html_soup = BeautifulSoup(response.text, 'html.parser')
 
-posts = html_soup.find_all('li', class_= 'result-row')
-print(type(posts)) #to double check that I got a ResultSet
-print(len(posts)) #to double check I got 120 (elements/page)
+def get_posts_on_page(url):
+    response = get('https://sfbay.craigslist.org/search/eby/fua')
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    posts = html_soup.find_all('li', class_= 'result-row')
+    return posts
+def get_price(post):
+    return int(post.a.text[2:])
+def get_description(post):
+    return post.find('a', class_='result-title hdrlnk').text
