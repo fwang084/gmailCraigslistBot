cc957a5e6c15367d852142e573aff67ffaccea3c
Frank Wang <frankw084@berkeley.edu>
2019-08-22 15:17:34 -0700

    ending_email function implemented

--- util.py
+++ util.py
@@ -7,11 +7,15 @@ def check_post(posts_to_email, post, items_budgets):
             posts_to_email.append(post)
             break
     return posts_to_email
-def send_posts(posts_to_email, sendInstance, url):
+def send_posts(posts_to_email, sendInstance, url, recipient):
     for post in posts_to_email:
-        message = sendInstance.create_message('frankw084084@gmail.com', 'frankw084084@gmail.com', cl.get_description(post), 'The price is ${price} at {url}'.format(price = cl.get_price(post), url = url))
+        message = sendInstance.create_message('frankw084084@gmail.com', recipient, cl.get_description(post), 'The price is ${price} at {url}'.format(price = cl.get_price(post), url = url))
         sendInstance.send_message('me', message)
-def check_for_failure(posts_to_email, sendInstance):
+def check_for_failure(posts_to_email, sendInstance, recipient):
     if len(posts_to_email) == 0:
-        message = sendInstance.create_message('frankw084084@gmail.com', 'frankw084084@gmail.com', 'Nothing new found', 'Will keep looking.')
+        message = sendInstance.create_message('frankw084084@gmail.com', recipient, 'Nothing new found', 'Will keep looking.')
         sendInstance.send_message('me', message)
+def ending_email(sendInstance, recipient):
+    message = sendInstance.create_message('frankw084084@gmail.com', recipient, 'Searching completed',
+                                          'Restart program to resume searching')
+    sendInstance.send_message('me', message)
