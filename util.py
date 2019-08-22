ad6cd76f3efbb56856f79f1612da68f776a1bdcc
Frank Wang <frankw084@berkeley.edu>
2019-08-22 15:07:37 -0700

    send_posts function changed to include url as input

--- util.py
+++ util.py
@@ -6,7 +6,7 @@ def check_post(posts_to_email, post, items_budgets):
         if item in cl.get_description(post) and cl.get_price(post) < items_budgets[item]:
             posts_to_email.append(post)
             break
-def send_posts(posts_to_email, sendInstance):
+def send_posts(posts_to_email, sendInstance, url):
     for post in posts_to_email:
         message = sendInstance.create_message('frankw084084@gmail.com', 'frankw084084@gmail.com', cl.get_description(post), 'The price is ${price} at {url}'.format(price = cl.get_price(post), url = url))
         sendInstance.send_message('me', message)
